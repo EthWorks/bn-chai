ebf58a18c2b6d7e60c886e1cf691d6e0097d73c9
Przemek <przemek@ethworks.io>
2018-03-12 12:27:14 +0100

    Changed invocation syntax. Updated readme.

--- index.js
+++ index.js
@@ -1,90 +1,80 @@
-module.exports.bnChaiInit = function(BN) {
-  const negativeProperty = function(actual) {
-    this.assert(actual.isNeg(),
-    'expected #{act} to be negative',
-    'expected #{act} to not be negative',
-    actual.toString(),
-    actual.toString());
-  }
-
-  const evenProperty = function(actual) {
-    this.assert(actual.isEven(),
-    'expected #{act} to be even',
-    'expected #{act} to not be even',
-    undefined,
-    actual.toString());
-  }
-
-  const oddProperty = function(actual) {
-    this.assert(actual.isOdd(),
-    'expected #{act} to be odd',
-    'expected #{act} to not be odd',
-    undefined,
-    actual.toString());
-  }
-
-  const zeroProperty = function(actual) {
-    this.assert(actual.isZero(),
-    'expected #{act} to be zero',
-    'expected #{act} to not be zero',
-    undefined,
-    actual.toString());
-  }
-
-  const equalMethod = function(expected, actual) {
-    this.assert(
-      actual.eq(expected),
-      'expected #{act} to equal #{exp}',
-      'expected #{act} to be different from #{exp}',
-      expected.toString(),
-      actual.toString()
-    );
-  }
-
-  const belowMethod = function(expected, actual) {
-    this.assert(
-      actual.lt(expected),
-      'expected #{act} to be below #{exp}',
-      'expected #{act} to not be below #{exp}',
-      expected.toString(),
-      actual.toString()
-    );
-  }
-
-  const mostMethod = function(expected, actual) {
-    this.assert(
-      actual.lte(expected),
-      'expected #{act} to be at most #{exp}',
-      'expected #{act} to not be at most #{exp}',
-      expected.toString(),
-      actual.toString()
-    );
-  }
-
-  const aboveMethod = function(expected, actual) {
-    this.assert(
-      actual.gt(expected),
-      'expected #{act} to be above #{exp}',
-      'expected #{act} to not be above #{exp}',
-      expected.toString(),
-      actual.toString()
-    );
-  }
-
-  const leastMethod = function(expected, actual) {
-    this.assert(
-      actual.gte(expected),
-      'expected #{act} to be at least #{exp}',
-      'expected #{act} to not be at least #{exp}',
-      expected.toString(),
-      actual.toString()
-    );
-  }
-
+module.exports = function(BN) {
   return function (chai, utils) {
-    chai.Assertion.addProperty('BN', function() {
-      utils.flag(this, 'BN', true);
-    });
+    const addFlag = function(flag) { return function() {utils.flag(this, flag, true);} }
+
+    chai.Assertion.addProperty('eq', addFlag('eq'));
+    chai.Assertion.addProperty('lt', addFlag('lt'));
+    chai.Assertion.addProperty('lte', addFlag('lte'));
+    chai.Assertion.addProperty('gt', addFlag('gt'));
+    chai.Assertion.addProperty('gte', addFlag('gte'));
+
+    const negativeProperty = function(actual) {
+      this.assert(actual.isNeg(),
+      `expected ${actual} to be negative`,
+      `expected ${actual} to not be negative`
+      );
+    }
+  
+    const evenProperty = function(actual) {
+      this.assert(actual.isEven(),
+      `expected ${actual} to be even`,
+      `expected ${actual} to not be even`,
+      );
+    }
+  
+    const oddProperty = function(actual) {
+      this.assert(actual.isOdd(),
+      `expected ${actual} to be odd`,
+      `expected ${actual} to not be odd`,
+      );
+    }
+  
+    const zeroProperty = function(actual) {
+      this.assert(actual.isZero(),
+      `expected ${actual} to be zero`,
+      `expected ${actual} to not be zero`,
+      );
+    }
+  
+    const equalMethod = function(expected, actual) {
+      this.assert(
+        actual.eq(expected),
+        `expected ${actual} to equal ${expected}`,
+        `expected ${actual} to be different from ${expected}`
+      );
+    }
+  
+    const belowMethod = function(expected, actual) {
+      this.assert(
+        actual.lt(expected),
+        `expected ${actual} to be below ${expected}`,
+        `expected ${actual} to not be below ${expected}`,
+      );
+    }
+  
+    const mostMethod = function(expected, actual) {
+      this.assert(
+        actual.lte(expected),
+        `expected ${actual} to be at most ${expected}`,
+        `expected ${actual} to not be at most ${expected}`,
+      );
+    }
+  
+    const aboveMethod = function(expected, actual) {
+      this.assert(
+        actual.gt(expected),
+        `expected ${actual} to be above ${expected}`,
+        `expected ${actual} to not be above ${expected}`,
+      );
+    }
+  
+    const leastMethod = function(expected, actual) {
+      this.assert(
+        actual.gte(expected),
+        `expected ${actual} to be at least ${expected}`,
+        `expected ${actual} to not be at least ${expected}`,
+      );
+    }
 
     const overrideFn = function(newFn) {
       return function (_super) {
@@ -100,25 +90,30 @@ module.exports.bnChaiInit = function(BN) {
 
     const addProp = function(newProp) {
       return function() {
-        const msg = 'BN flag is not set. Did you forget (...).BN.(...) ?';
-        if (utils.flag(this, 'negate')) {
-          this.assert(!utils.flag(this, 'BN'), msg, msg);
-        } else {
-          this.assert(utils.flag(this, 'BN'), msg, msg);
-        }
         newProp.apply(this, [new BN(this._obj)]);
       }
     }
+
+    const BNMethod = function(expected) {
+      const actual = new BN(this._obj);
+      expected = new BN(expected);
+      if (utils.flag(this, 'eq')) equalMethod.apply(this, [expected, actual]);
+      if (utils.flag(this, 'lt')) belowMethod.apply(this, [expected, actual]);
+      if (utils.flag(this, 'lte')) mostMethod.apply(this, [expected, actual]);
+      if (utils.flag(this, 'gt')) aboveMethod.apply(this, [expected, actual]);
+      if (utils.flag(this, 'gte')) leastMethod.apply(this, [expected, actual]);
+      if (!utils.flag(this, 'eq') && !utils.flag(this, 'lt') && !utils.flag(this, 'lte') &&
+                                  !utils.flag(this, 'gt') && !utils.flag(this, 'gte')) {
+        const msg = 'Property missing. Available: eq, lt, lte, gt, gte.';
+        this.assert(utils.flag(this, 'negate'), msg, msg);
+      }
+    }
+
+    chai.Assertion.addMethod('BN', BNMethod);
 
     chai.Assertion.addProperty('negative', addProp(negativeProperty))
     chai.Assertion.addProperty('even', addProp(evenProperty));
     chai.Assertion.addProperty('odd', addProp(oddProperty));
     chai.Assertion.addProperty('zero', addProp(zeroProperty));
-
-    chai.Assertion.overwriteMethod('equal', overrideFn(equalMethod));
-    chai.Assertion.overwriteMethod('below', overrideFn(belowMethod));
-    chai.Assertion.overwriteMethod('most', overrideFn(mostMethod));
-    chai.Assertion.overwriteMethod('above', overrideFn(aboveMethod));
-    chai.Assertion.overwriteMethod('least', overrideFn(leastMethod));
   };
 };
